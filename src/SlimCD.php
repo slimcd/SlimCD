a4170f2d6344b5897bc193395444f79e8fe43718
levi durfee <ldurfee@bulldogcreative.com>
2016-11-02 11:43:26 -0400

    Moved protected methods to the bottom and fixed an property mismatch for ShowSessionRequest

--- src/SlimCD.php
+++ src/SlimCD.php
@@ -56,92 +56,6 @@ class SlimCD
     private $senddata;
     private $recvdata;
 
-    function StandardErrorBlock($url, $errortext)
-    {
-        $reply = (object) array('response'=>'Error', 'responsecode' => '2', 'description' => $errortext , 'responseurl' => $url ,'datablock' => '');
-        $result = (object) array('reply' => $reply) ;
-
-        return ($result ) ;
-    }
-
-    function HttpPost($urlString, $timeout, $namevalueArray)
-    {
-        $ch = curl_init($urlString);
-
-        curl_setopt($ch,CURLOPT_TIMEOUT,$timeout) ;
-        curl_setopt($ch, CURLOPT_POST, 1);
-
-        $this->senddata = http_build_query($namevalueArray) ;
-
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $this->senddata);
-
-        // SLIMCD.COM uses a GODADDY SSL certificate.  Once you install the CA for GoDaddy SSL, please remove the line below.
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-
-        // receive server response ...
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
-
-        // POST the data
-        $this->recvdata = curl_exec($ch);
-
-        if(curl_errno($ch)) {
-            $result = $this->StandardErrorBlock(curl_getinfo($ch, CURLINFO_EFFECTIVE_URL), curl_error($ch));
-        } else {
-
-            $httpstatus = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-            $contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
-
-            if (intval($httpstatus) !== 200 || ($contentType !== 'application/json' && $contentType !== 'text/javascript')) {
-                $result =  $this->StandardErrorBlock(curl_getinfo($ch, CURLINFO_EFFECTIVE_URL), $this->recvdata) ;
-            } else {
-                $result = json_decode($this->recvdata);
-            }
-
-            // Make sure we can decode the results...
-            if($result === null) {
-                switch (json_last_error()) {
-                    case JSON_ERROR_NONE:
-                        $errortext= ' - No errors';
-                        break;
-                    case JSON_ERROR_DEPTH:
-                        $errortext = ' - Maximum stack depth exceeded';
-                        break;
-                    case JSON_ERROR_STATE_MISMATCH:
-                        $errortext = ' - Underflow or the modes mismatch';
-                        break;
-                    case JSON_ERROR_CTRL_CHAR:
-                        $errortext = ' - Unexpected control character found';
-                        break;
-                    case JSON_ERROR_SYNTAX:
-                        $errortext= ' - Syntax error, malformed JSON';
-                        break;
-                    case JSON_ERROR_UTF8:
-                        $errortext = ' - Malformed UTF-8 characters, possibly incorrectly encoded';
-                        break;
-                    default:
-                        $errortext = ' - Unknown JSON error';
-                        break;
-                }
-                $result = $this->StandardErrorBlock($urlString, $errortext);
-            }
-        }
-
-        curl_close ($ch);
-
-        // flatten out the "reply" so we don't have that extra (unneeded) level
-        $myarray = get_object_vars($result->reply);
-        if($this->debug) {
-            $myarray = array_merge($myarray, array("senddata" => $this->senddata , "recvdata" => $this->recvdata));
-        }
-        $result = (object) $myarray;
-
-        $this->senddata = '';
-        $this->recvdata = '';
-
-        return $result ;
-    }
-
     /* Images Methods */
 
     function Images_DownloadSignature(DownloadSignatureRequest $request, $timeout = false)
@@ -597,7 +511,7 @@ class SlimCD
 
     function Sessions_ShowSession(ShowSessionRequest $request)
     {
-        return ( $this->statsURL . "/soft/showsession.asp?sessionid=" . urlencode($request->SessionID) );
+        return ($this->statsURL . "/soft/showsession.asp?sessionid=" . urlencode($request->sessionid));
     }
 
     function Sessions_SpawnSessions(SpawnSessionsRequest $request, $timeout = false)
@@ -648,5 +562,91 @@ class SlimCD
         }
 
         return ($this->HttpPost($this->transURL . "/soft/json/jsonscript.asp?service=CloseBatch", $timeout, $request->jsonSerialize()));
+    }
+
+    protected function StandardErrorBlock($url, $errortext)
+    {
+        $reply = (object) array('response'=>'Error', 'responsecode' => '2', 'description' => $errortext , 'responseurl' => $url ,'datablock' => '');
+        $result = (object) array('reply' => $reply) ;
+
+        return ($result ) ;
+    }
+
+    protected function HttpPost($urlString, $timeout, $namevalueArray)
+    {
+        $ch = curl_init($urlString);
+
+        curl_setopt($ch,CURLOPT_TIMEOUT,$timeout) ;
+        curl_setopt($ch, CURLOPT_POST, 1);
+
+        $this->senddata = http_build_query($namevalueArray) ;
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $this->senddata);
+
+        // SLIMCD.COM uses a GODADDY SSL certificate.  Once you install the CA for GoDaddy SSL, please remove the line below.
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+
+        // receive server response ...
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
+
+        // POST the data
+        $this->recvdata = curl_exec($ch);
+
+        if(curl_errno($ch)) {
+            $result = $this->StandardErrorBlock(curl_getinfo($ch, CURLINFO_EFFECTIVE_URL), curl_error($ch));
+        } else {
+
+            $httpstatus = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            $contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+
+            if (intval($httpstatus) !== 200 || ($contentType !== 'application/json' && $contentType !== 'text/javascript')) {
+                $result =  $this->StandardErrorBlock(curl_getinfo($ch, CURLINFO_EFFECTIVE_URL), $this->recvdata) ;
+            } else {
+                $result = json_decode($this->recvdata);
+            }
+
+            // Make sure we can decode the results...
+            if($result === null) {
+                switch (json_last_error()) {
+                    case JSON_ERROR_NONE:
+                        $errortext= ' - No errors';
+                        break;
+                    case JSON_ERROR_DEPTH:
+                        $errortext = ' - Maximum stack depth exceeded';
+                        break;
+                    case JSON_ERROR_STATE_MISMATCH:
+                        $errortext = ' - Underflow or the modes mismatch';
+                        break;
+                    case JSON_ERROR_CTRL_CHAR:
+                        $errortext = ' - Unexpected control character found';
+                        break;
+                    case JSON_ERROR_SYNTAX:
+                        $errortext= ' - Syntax error, malformed JSON';
+                        break;
+                    case JSON_ERROR_UTF8:
+                        $errortext = ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+                        break;
+                    default:
+                        $errortext = ' - Unknown JSON error';
+                        break;
+                }
+                $result = $this->StandardErrorBlock($urlString, $errortext);
+            }
+        }
+
+        curl_close ($ch);
+
+        // flatten out the "reply" so we don't have that extra (unneeded) level
+        $myarray = get_object_vars($result->reply);
+        if($this->debug) {
+            $myarray = array_merge($myarray, array("senddata" => $this->senddata , "recvdata" => $this->recvdata));
+        }
+        $result = (object) $myarray;
+
+        $this->senddata = '';
+        $this->recvdata = '';
+
+        return $result ;
     }
 }
