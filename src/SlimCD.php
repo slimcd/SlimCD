40431f2028f1b43a2612a02dbc492a9cc689de47
Levi <levi.durfee@gmail.com>
2016-11-02 20:20:36 -0400

    tidying up the code

--- src/SlimCD.php
+++ src/SlimCD.php
@@ -49,9 +49,14 @@ abstract class SlimCD implements Interfaces\SlimCD
      */
     protected function errorBlock($url, $errorMessage)
     {
-        $reply = (object) array('response' => 'Error', 'responsecode' => '2', 'description' => $errorMessage,
-                                'responseurl' => $url,'datablock' => '');
-        $result = (object) array('reply' => $reply) ;
+        $reply = (object) array(
+            'response'     => 'Error',
+            'responsecode' => '2',
+            'description'  => $errorMessage,
+            'responseurl'  => $url,
+            'datablock'    => ''
+        );
+        $result = (object) array('reply' => $reply);
 
         return ($result);
     }
@@ -66,14 +71,15 @@ abstract class SlimCD implements Interfaces\SlimCD
     {
         $ch = curl_init($urlString);
 
-        curl_setopt($ch,CURLOPT_TIMEOUT, $timeout) ;
+        curl_setopt($ch,CURLOPT_TIMEOUT, $timeout);
         curl_setopt($ch, CURLOPT_POST, 1);
 
         $this->send = http_build_query($nameValueArray) ;
 
         curl_setopt($ch, CURLOPT_POSTFIELDS, $this->send);
 
-        // SLIMCD.COM uses a GODADDY SSL certificate.  Once you install the CA for GoDaddy SSL, please remove the line below.
+        // SLIMCD.COM uses a GODADDY SSL certificate.  Once you install the CA for GoDaddy SSL, please
+        // remove the line below.
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 
         // receive server response ...
@@ -90,7 +96,8 @@ abstract class SlimCD implements Interfaces\SlimCD
             $httpstatus = curl_getinfo($ch, CURLINFO_HTTP_CODE);
             $contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
 
-            if (intval($httpstatus) !== 200 || ($contentType !== 'application/json' && $contentType !== 'text/javascript')) {
+            if (intval($httpstatus) !== 200 || ($contentType !== 'application/json'
+                    && $contentType !== 'text/javascript')) {
                 $result =  $this->errorBlock(curl_getinfo($ch, CURLINFO_EFFECTIVE_URL), $this->receive) ;
             } else {
                 $result = json_decode($this->receive);
@@ -100,44 +107,46 @@ abstract class SlimCD implements Interfaces\SlimCD
             if($result === null) {
                 switch (json_last_error()) {
                     case JSON_ERROR_NONE:
-                        $errortext= ' - No errors';
+                        $errorMessage= ' - No errors';
                         break;
                     case JSON_ERROR_DEPTH:
-                        $errortext = ' - Maximum stack depth exceeded';
+                        $errorMessage = ' - Maximum stack depth exceeded';
                         break;
                     case JSON_ERROR_STATE_MISMATCH:
-                        $errortext = ' - Underflow or the modes mismatch';
+                        $errorMessage = ' - Underflow or the modes mismatch';
                         break;
                     case JSON_ERROR_CTRL_CHAR:
-                        $errortext = ' - Unexpected control character found';
+                        $errorMessage = ' - Unexpected control character found';
                         break;
                     case JSON_ERROR_SYNTAX:
-                        $errortext= ' - Syntax error, malformed JSON';
+                        $errorMessage= ' - Syntax error, malformed JSON';
                         break;
                     case JSON_ERROR_UTF8:
-                        $errortext = ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+                        $errorMessage = ' - Malformed UTF-8 characters, possibly incorrectly encoded';
                         break;
                     default:
-                        $errortext = ' - Unknown JSON error';
+                        $errorMessage = ' - Unknown JSON error';
                         break;
                 }
-                $result = $this->errorBlock($urlString, $errortext);
+                $result = $this->errorBlock($urlString, $errorMessage);
             }
         }
 
         curl_close ($ch);
 
         // flatten out the "reply" so we don't have that extra (unneeded) level
-        $myarray = get_object_vars($result->reply);
+        $replyResult = get_object_vars($result->reply);
         if($this->debug) {
-            $myarray = array_merge($myarray, array("senddata" => $this->send , "recvdata" => $this->receive));
+            $replyResult = array_merge($replyResult,
+                array("senddata" => $this->send , "recvdata" => $this->receive)
+            );
         }
-        $result = (object) $myarray;
+        $result = (object) $replyResult;
 
         $this->send = '';
         $this->receive = '';
 
-        return $result ;
+        return $result;
     }
 
     /**
@@ -150,9 +159,6 @@ abstract class SlimCD implements Interfaces\SlimCD
             $timeout = $this->defaultTimeout;
         } else {
             $timeout = intval($timeout);
-            if($timeout === 0) {
-                $timeout = $this->defaultTimeout;
-            }
         }
         return $timeout;
     }
