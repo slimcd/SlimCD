370485aa9d846c1ec292dceb09dec707fa2bbe86
Levi <levi.durfee@gmail.com>
2016-11-03 21:08:51 -0400

    guzzle is working and most tests should still work

--- src/SlimCD.php
+++ src/SlimCD.php
@@ -2,6 +2,8 @@
 
 namespace SlimCD;
 
+use GuzzleHttp\Client;
+
 /**
  * Class SlimCD
  * @package SlimCD
@@ -70,89 +72,48 @@ abstract class SlimCD implements Interfaces\SlimCD
      */
     protected function httpPost($urlString, $timeout, $nameValueArray)
     {
-        $curlHandler = curl_init($urlString);
+        $client = new Client(['timeout' => $timeout]);
+        $response = $client->request('POST', $urlString, [
+            'query' => $nameValueArray
+        ]);
+        $code = $response->getStatusCode();
+        $contentType = $response->getHeader('Content-Type')[0];
 
-        curl_setopt($curlHandler,CURLOPT_TIMEOUT, $timeout);
-        curl_setopt($curlHandler, CURLOPT_POST, 1);
-
-        $this->send = http_build_query($nameValueArray) ;
-
-        curl_setopt($curlHandler, CURLOPT_POSTFIELDS, $this->send);
-
-        // SLIMCD.COM uses a GODADDY SSL certificate.  Once you install the CA for GoDaddy SSL, please
-        // remove the line below.
-        curl_setopt($curlHandler, CURLOPT_SSL_VERIFYPEER, 0);
-
-        // receive server response ...
-        curl_setopt($curlHandler, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curlHandler, CURLOPT_FOLLOWLOCATION, 0);
-
-        // POST the data
-        $this->receive = curl_exec($curlHandler);
-        // @codeCoverageIgnoreStart
-        if(curl_errno($curlHandler)) {
-            $this->buildError(
-                curl_getinfo($curlHandler, CURLINFO_EFFECTIVE_URL),
-                curl_error($curlHandler)
-            );
-            throw new \Exception('Curl Error');
+        if($code !== 200 || ($contentType !== 'application/json' && $contentType !== 'text/javascript')) {
+            throw new \Exception("Not okay");
         } else {
-
-            $httpStatus = curl_getinfo($curlHandler, CURLINFO_HTTP_CODE);
-            $contentType = curl_getinfo($curlHandler, CURLINFO_CONTENT_TYPE);
-
-            if (intval($httpStatus) !== 200 || ($contentType !== 'application/json'
-                    && $contentType !== 'text/javascript')) {
-                $result =  $this->buildError(curl_getinfo($curlHandler, CURLINFO_EFFECTIVE_URL), $this->receive);
-            } else {
-                $result = json_decode($this->receive);
-            }
-
-            // Make sure we can decode the results...
-            if($result === null) {
-                switch (json_last_error()) {
-                    case JSON_ERROR_NONE:
-                        $errorMessage= ' - No errors';
-                        break;
-                    case JSON_ERROR_DEPTH:
-                        $errorMessage = ' - Maximum stack depth exceeded';
-                        break;
-                    case JSON_ERROR_STATE_MISMATCH:
-                        $errorMessage = ' - Underflow or the modes mismatch';
-                        break;
-                    case JSON_ERROR_CTRL_CHAR:
-                        $errorMessage = ' - Unexpected control character found';
-                        break;
-                    case JSON_ERROR_SYNTAX:
-                        $errorMessage= ' - Syntax error, malformed JSON';
-                        break;
-                    case JSON_ERROR_UTF8:
-                        $errorMessage = ' - Malformed UTF-8 characters, possibly incorrectly encoded';
-                        break;
-                    default:
-                        $errorMessage = ' - Unknown JSON error';
-                        break;
-                }
-                $result = $this->buildError($urlString, $errorMessage);
-            }
+            $jsonBody = $response->getBody();
+            $result = json_decode($jsonBody);
         }
-        // @codeCoverageIgnoreEnd
 
-        curl_close ($curlHandler);
-
-        // flatten out the "reply" so we don't have that extra (unneeded) level
-        $replyResult = get_object_vars($result->reply);
-        if($this->debug) {
-            $replyResult = array_merge($replyResult,
-                array("senddata" => $this->send , "recvdata" => $this->receive)
-            );
+        if($result === null) {
+            switch (json_last_error()) {
+                case JSON_ERROR_NONE:
+                    $errorMessage= ' - No errors';
+                    break;
+                case JSON_ERROR_DEPTH:
+                    $errorMessage = ' - Maximum stack depth exceeded';
+                    break;
+                case JSON_ERROR_STATE_MISMATCH:
+                    $errorMessage = ' - Underflow or the modes mismatch';
+                    break;
+                case JSON_ERROR_CTRL_CHAR:
+                    $errorMessage = ' - Unexpected control character found';
+                    break;
+                case JSON_ERROR_SYNTAX:
+                    $errorMessage= ' - Syntax error, malformed JSON';
+                    break;
+                case JSON_ERROR_UTF8:
+                    $errorMessage = ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+                    break;
+                default:
+                    $errorMessage = ' - Unknown JSON error';
+                    break;
+            }
+            throw new \Exception("Json Error " . $errorMessage);
         }
-        $result = (object) $replyResult;
 
-        $this->send = '';
-        $this->receive = '';
-
-        return $result;
+        return $result->reply;
     }
 
     /**
